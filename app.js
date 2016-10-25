d2a429957f35f9d4144acb88250ae0bfcf9c641e
Olexandr Kushnir <olexandrrazor@mail.ru>
2016-10-25 19:29:07 +0300

    Added 'create campaign view'

--- app.js
+++ app.js
@@ -5,25 +5,34 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.get('/', function(req, res) {
+  res.render('campaign_created')
+});
+
+// app.get('/app', function(req, res) {
+//   var data='test data';
+//   res.render('index', {
+//
+//   });
+// });
+
+
+
+app.listen(8080);
+console.log('8080 is the magic port');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,6 +64,3 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-
-module.exports = app;
